4b066a0fb562c37c396c8396e203fdb53ad694ed
Giacomo Scampini <scampini.giacomo@gmail.com>
2023-01-02 19:30:50 +0100

    prime parti dell'interfaccia completate

--- src/json/sillabe.ts
+++ src/json/sillabe.ts
@@ -1,3 +1,26 @@
-let sillabe = [{}];
+interface ParolaESillabe {
+    parola: string,
+    sillabe: string
+};
+
+let sillabe: ParolaESillabe[] = [
+    {
+        parola: "Caiga",
+        sillabe: "cai - ga"
+    },
+    {
+        parola: "Fuera",
+        sillabe: "fue - ra"
+    },
+    {
+        parola: "Fuimos",
+        sillabe: "fui - mos"
+    },
+    {
+        parola: "Canción",
+        sillabe: "can - ción"
+    },
+];
 
 export default sillabe;
+export type { ParolaESillabe };
--- src/views/SillabeView.vue
+++ src/views/SillabeView.vue
@@ -1,9 +1,78 @@
-<script setup lang="ts">
-import sillabe from '@/json/sillabe';
+<script lang="ts">
+import sillabeOriginali from '@/json/sillabe';
+import type { ParolaESillabe } from '@/json/sillabe';
+import shuffle from '@/data/shuffle';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    let g: ParolaESillabe = { parola:'', sillabe:'' };
+    return {
+      parolaAttuale: g,
+      sillabeUtente: '',
+      so: sillabeOriginali,
+      sillabeDaTestare: shuffle(sillabeOriginali)
+    }
+  },
+  created() {
+    this.aggiornaParola();
+  },
+  methods: {
+    aggiornaParola() {
+      if (this.sillabeDaTestare.length <= 0) {
+        this.parolaAttuale.parola = 'Tutto testato.';
+        return;
+      }
+      this.parolaAttuale = {
+        parola: this.sillabeDaTestare[0].parola,
+        sillabe: this.sillabeDaTestare[0].sillabe
+      }
+      this.sillabeUtente = '';
+    },
+    test(event: Event) {
+      if (this.sillabeDaTestare.length <= 0) {
+        this.parolaAttuale.parola = 'Tutto testato.';
+        return;
+      }
+      if (this.sillabeUtente === this.sillabeDaTestare[0].sillabe) {
+        // caso input utente giusto
+        this.sillabeDaTestare.shift();
+      }
+      else {
+        // caso input utente sbagliato
+        let max = this.sillabeDaTestare.length > 5 ? 5 : this.sillabeDaTestare.length;
+        let randomIndex = Math.floor(Math.random() * max) + 1;
+        console.log(randomIndex);
+        this.sillabeDaTestare.splice(randomIndex, 0, this.parolaAttuale);
+        this.sillabeDaTestare.shift();
+      }
+      this.aggiornaParola();
+    }
+  }
+})
 </script>
 
 <template>
   <main>
-    {{ sillabe }}
+    <div class="form">
+      {{ parolaAttuale.parola }} <br/>
+      <input v-model.trim="sillabeUtente" @keyup.enter="test">
+      <button @click="test">Test</button>
+    </div>
+    <div class="sillabe">
+      {{ so }}
+    </div>
+    <div class="sillabe">
+      {{ sillabeDaTestare }}
+    </div>
   </main>
 </template>
+
+<style scoped>
+.form {
+  padding-top: 20px;
+}
+.sillabe {
+  padding-top: 20px;
+}
+</style>
