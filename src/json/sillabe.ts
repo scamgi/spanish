3343f462aef240e9df2879f0ebbdb0141f442b62
Giacomo Scampini <scampini.giacomo@gmail.com>
2023-01-02 18:02:08 +0100

    sillabe view added

---
+++ src/json/sillabe.ts
@@ -0,0 +1,3 @@
+let sillabe = [{}];
+
+export default sillabe;
---
+++ src/views/SillabeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import sillabe from '@/json/sillabe';
+</script>
+
+<template>
+  <main>
+    {{ sillabe }}
+  </main>
+</template>
